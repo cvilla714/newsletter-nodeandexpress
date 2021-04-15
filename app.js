43a7a360c749a80095c4dc5692713c78ec979d2b
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-15 13:26:49 -0400

    added the post request and logged in the values coming from the form

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body.firstname);
+  console.log(req.body.lastname);
+  console.log(req.body.email);
 });
 
 app.listen(port, () => {
